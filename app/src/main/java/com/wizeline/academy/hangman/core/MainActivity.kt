c47310bb288d7255183db4feadba1b9516ef43f2
Victor Herrera <victor.herrera@wizeline.com>
2022-07-27 19:54:53 -0500

    App: moved MainActivity

--- app/src/main/java/com/wizeline/academy/hangman/core/MainActivity.kt
+++ app/src/main/java/com/wizeline/academy/hangman/core/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.wizeline.academy.hangman
+package com.wizeline.academy.hangman.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wizeline.academy.hangman.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
