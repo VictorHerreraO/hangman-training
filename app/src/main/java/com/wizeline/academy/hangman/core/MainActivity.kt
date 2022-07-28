d181e429e9ea8ac5165a408ff9bf2aa076700efe
Victor Herrera <victor.herrera@wizeline.com>
2022-07-27 20:00:45 -0500

    App: configured hilt

--- app/src/main/java/com/wizeline/academy/hangman/core/MainActivity.kt
+++ app/src/main/java/com/wizeline/academy/hangman/core/MainActivity.kt
@@ -3,7 +3,9 @@ package com.wizeline.academy.hangman.core
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wizeline.academy.hangman.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
