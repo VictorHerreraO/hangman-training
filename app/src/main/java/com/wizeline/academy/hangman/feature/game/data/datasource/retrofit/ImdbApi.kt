97ca50026faef4c54d9b018d3ef53af9071b77f8
Victor Herrera <victor.herrera@wizeline.com>
2022-07-30 19:38:33 -0500

    Game: added retrofit API

---
+++ app/src/main/java/com/wizeline/academy/hangman/feature/game/data/datasource/retrofit/ImdbApi.kt
@@ -0,0 +1,9 @@
+package com.wizeline.academy.hangman.feature.game.data.datasource.retrofit
+
+object ImdbApi {
+    const val BASE_URL = "https://api.themoviedb.org/3"
+
+    object Endpoint {
+        const val POPULAR_MOVIES = "/movie/popular"
+    }
+}
