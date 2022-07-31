c091959b88cce7cf3b25f914a9bb9fbd9bcd2754
Victor Herrera <victor.herrera@wizeline.com>
2022-07-30 19:50:39 -0500

    Game: added movie dto to challenge model mapper

--- app/src/main/java/com/wizeline/academy/hangman/feature/game/data/repository/impl/ChallengeRepository.kt
+++ app/src/main/java/com/wizeline/academy/hangman/feature/game/data/repository/impl/ChallengeRepository.kt
@@ -1,19 +1,22 @@
 package com.wizeline.academy.hangman.feature.game.data.repository.impl
 
+import com.soyvictorherrera.bdates.core.arch.Mapper
 import com.wizeline.academy.hangman.feature.game.ChallengeModel
 import com.wizeline.academy.hangman.feature.game.data.datasource.MoviesRemoteDataSourceContract
+import com.wizeline.academy.hangman.feature.game.data.datasource.retrofit.MovieDto
 import com.wizeline.academy.hangman.feature.game.data.repository.ChallengeRepositoryContract
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class ChallengeRepository @Inject constructor(
-    private val moviesRemoteDataSource: MoviesRemoteDataSourceContract
+    private val moviesRemoteDataSource: MoviesRemoteDataSourceContract,
+    private val movieDtoToChallengeModelMapper: Mapper<MovieDto, ChallengeModel>
 ) : ChallengeRepositoryContract {
 
     override fun getRandomChallenge(): Single<ChallengeModel> {
         return moviesRemoteDataSource.getRandomPopularMovies()
             .map { list -> list.random() }
-            .map { movie -> ChallengeModel(movie.title!!) }
+            .map(movieDtoToChallengeModelMapper::map)
     }
 
 }
