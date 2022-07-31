b661063dbaaa710638d2db832fe5a5740c7ab6d5
Victor Herrera <victor.herrera@wizeline.com>
2022-07-30 19:41:20 -0500

    Game: added game hilt module

---
+++ app/src/main/java/com/wizeline/academy/hangman/feature/game/data/di/GameModule.kt
@@ -0,0 +1,64 @@
+package com.wizeline.academy.hangman.feature.game.data.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.wizeline.academy.hangman.BuildConfig
+import com.wizeline.academy.hangman.feature.game.data.datasource.retrofit.ImdbApi
+import com.wizeline.academy.hangman.feature.game.data.datasource.retrofit.MoviesClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Named
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class GameModule {
+
+    private companion object {
+        const val LOGGING_INTERCEPTOR = "logging_interceptor"
+    }
+
+    @Provides
+    fun provideGson(
+
+    ): Gson = GsonBuilder().create()
+
+    @[Provides Named(LOGGING_INTERCEPTOR)]
+    fun provideLoggingInterceptor(
+
+    ): Interceptor = HttpLoggingInterceptor().setLevel(
+        HttpLoggingInterceptor.Level.BODY
+    )
+
+    @Provides
+    fun provideOkHttpClient(
+        @Named(LOGGING_INTERCEPTOR) loggingInterceptor: Interceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .apply {
+            if (BuildConfig.DEBUG) addInterceptor(loggingInterceptor)
+        }
+        .build()
+
+    @Provides
+    fun provideRetrofit(
+        gson: Gson,
+        okHttpClient: OkHttpClient
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(ImdbApi.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    fun provideMoviesClient(
+        retrofit: Retrofit
+    ): MoviesClient = retrofit.create()
+
+}
